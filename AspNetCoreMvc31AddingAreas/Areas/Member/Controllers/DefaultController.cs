9b81993dd174365de0b2b1e78c281ee57579e802
johnny <johnny@wantgoo.com>
2020-05-11 10:21:17 +0800

    first commit

---
+++ AspNetCoreMvc31AddingAreas/Areas/Member/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvc31AddingAreas.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
